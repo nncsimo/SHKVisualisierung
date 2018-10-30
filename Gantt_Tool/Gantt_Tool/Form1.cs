ebbf689c9435b3493f376954fdfdde35d8e00794
nncismo <nils.cosmus@gmail.com>
2018-10-30 10:43:46 +0100

    OxyPlot example

--- Gantt_Tool/Gantt_Tool/Form1.cs
+++ Gantt_Tool/Gantt_Tool/Form1.cs
@@ -8,13 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Gantt_Tool
 {
+    using OxyPlot;
+    using OxyPlot.Series;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            var myModel = new PlotModel { Title = "Example 1" };
+            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            this.plot1.Model = myModel;
         }
+
     }
 }
