87325ca0b60a4ac88c86271568253745dc40d58a
nncismo <nils.cosmus@gmail.com>
2018-12-08 13:05:49 +0100

    Versuche

--- Gantt_Tool/Gantt_Tool/ReadModel.Designer.cs
+++ Gantt_Tool/Gantt_Tool/ReadModel.Designer.cs
@@ -28,13 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 151);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 65);
             this.button1.TabIndex = 0;
@@ -42,16 +47,41 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(154, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.RangeBar;
+            series1.CustomProperties = "BarLabelStyle=Left";
+            series1.Legend = "Legend1";
+            series1.Name = "Data1";
+            series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int64;
+            series1.YValuesPerPoint = 2;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int64;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1082, 651);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // ReadModel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1248, 675);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button1);
             this.Name = "ReadModel";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.ReadModel_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +90,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Gantt_Tool/Gantt_Tool/ReadModel.cs
+++ Gantt_Tool/Gantt_Tool/ReadModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Gantt_Tool
 {
@@ -19,7 +20,7 @@ namespace Gantt_Tool
 
         private void ReadModel_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,7 +28,46 @@ namespace Gantt_Tool
             openFileDialog1.ShowDialog();
             //string filedirectory = openFileDialog1.FileName;
             ScheduleData SData = new ScheduleData(openFileDialog1.FileName);
+
+            CreateChart(SData);
         }
-        
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateChart(ScheduleData initData)
+        {
+            //    this.chart1.Series.Clear();
+            int j = 1;
+            chart1.Titles.Add("Activities");
+
+            for (int i = 0; i < initData.Makespan; i++)
+            {
+                chart1.Series["Data1"].Points.AddXY(i, initData.ResourceConsumptionAtTime[j,i]);
+            }            
+            
+            //chart1.Series["Data1"].Points.AddXY("0", "2");
+            //chart1.Series["Data1"].Points.AddXY("1", "2");
+            //chart1.Series["Data1"].Points.AddXY("2", "0");
+
+            //// Data arrays
+            //string[] seriesArray = { "Cat" };
+            //int[] pointsArray = { 2 };
+
+            //// Set palette
+            //this.chart1.Palette = ChartColorPalette.EarthTones;
+
+            //// Set title
+            //this.chart1.Titles.Add("Activities");
+
+            //// Add series.
+            //for (int i = 0; i < seriesArray.Length; i++)
+            //{
+            //    Series series = this.chart1.Series.Add(seriesArray[i]);
+            //    series.Points.Add(pointsArray[i]);
+            //}
+        }
     }
 }
