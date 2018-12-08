7fe4d405e9cc7b8d4ceb94f96fbbab3bcac77ad4
nncismo <nils.cosmus@gmail.com>
2018-12-08 17:50:14 +0100

    Erste Visualisierung

--- Gantt_Tool/Gantt_Tool/ReadModel.cs
+++ Gantt_Tool/Gantt_Tool/ReadModel.cs
@@ -21,7 +21,7 @@ namespace Gantt_Tool
         private void ReadModel_Load(object sender, EventArgs e)
         {
             
-        }
+        }      
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -34,29 +34,17 @@ namespace Gantt_Tool
 
         private void chart1_Click(object sender, EventArgs e)
         {
-
-        }
+                   }
 
         private void CreateChart(ScheduleData initData)
         {
-            // short reference for our dummy:
-            Series S0 = chart1.Series[0];
-            // a simple type
-            S0.ChartType = SeriesChartType.Line;
-            // set 10 point with x-values going from 0-100 and y-values going from 1-10:
-            for (int i = 0; i < 100; i += 10) S0.Points.AddXY(i, i / 10);
-            // or add only a few, e.g. the first and last points:
-            //S0.Points.AddXY(100, 10);
-            //S0.Points.AddXY(0, 10);
-            // hide the line:
-            S0.Color = Color.Transparent;
-            // hide the legend text (it will still take up a little space, though)
-            S0.LegendText = " ";
-            // limit the axis to the target values
-            chart1.ChartAreas[0].AxisX.Maximum = 100;
-            chart1.ChartAreas[0].AxisX.Minimum = 0;
-
-
+            for (int i = 0; i < initData.NumberOfActivities; i++)
+            {
+                chart1.Series.Add($"a{i}");
+                chart1.Series[$"a{i}"].Points.Add(new DataPoint(initData.ListOfActivities[i].startingTime, initData.ListOfActivities[i].renewableResourceConsumption[1]));
+                chart1.Series[$"a{i}"].Points.Add(new DataPoint(initData.ListOfActivities[i].finishTime, initData.ListOfActivities[i].renewableResourceConsumption[1]));
+                chart1.Series[$"a{i}"].ChartType = SeriesChartType.StackedArea;
+            }
         }
     }
 }
